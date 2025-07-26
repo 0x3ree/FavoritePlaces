6d158294e89445e9e1b6c24437a9dc31c1c78b6e
Three <ottunade95@gmail.com>
2025-07-26 13:42:43 +0100

    Implement navigation and image picking features; add screens and components for managing favorite places

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import AllPlaces from "./screens/AllPlaces";
+import AddPlace from "./screens/AddPlace";
+import IconButton from "./components/UI/IconButton";
+import { Colors } from "./constants/colors";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: Colors.primary500 },
+            headerTintColor: Colors.gray700,
+            contentStyle: { backgroundColor: Colors.gray700 },
+          }}
+        >
+          <Stack.Screen
+            name="AllPlaces"
+            component={AllPlaces}
+            options={({ navigation }) => ({
+              title: "Your Favorite Places",
+              headerRight: ({ tintColor }) => (
+                <IconButton
+                  icon="add-to-list"
+                  size={24}
+                  color={tintColor || Colors.accent500}
+                  onPress={() => navigation.navigate("AddPlace")}
+                />
+              ),
+            })}
+          />
+          <Stack.Screen
+            name="AddPlace"
+            component={AddPlace}
+            options={{
+              title: "Add a new Place",
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+    //  //instead of using an inline function in the onPress prop,we use an inine fuction in the options prop using the navigate method(we get from being a screen) to navigate to the addPlace screen, rather than calling the useNavigation hook
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Places/PlaceForm.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { View, Text, ScrollView, TextInput, StyleSheet } from "react-native";
+import { Colors } from "../../constants/colors";
+import ImagePicker from "./ImagePicker";
+
+function PlaceForm(enteredText) {
+  const [enteredTitle, setEnteredTitle] = useState("");
+
+  function changeTitleHandler(enteredText) {
+    setEnteredTitle(enteredText); // updates the state with the text entered in the textInput, the enteredText is the text entered by the user and it's given by react native.
+  }
+  return (
+    <ScrollView style={styles.form}>
+      <View>
+        <Text style={styles.label}> Title</Text>
+        <TextInput
+          onChange={changeTitleHandler}
+          value={enteredTitle}
+          style={styles.input}
+        />
+      </View>
+      <ImagePicker />
+    </ScrollView>
+  ); // we use the onChange to listen to the chnages in the textInput and update the state with the setEnteredText ()
+}
+
+export default PlaceForm;
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: "bold",
+    marginBottom: 4,
+    color: Colors.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: Colors.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: Colors.primary100,
+  },
+});
